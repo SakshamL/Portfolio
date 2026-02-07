f0190e895fdf9374e7408ac3c5d05419e47d9a71
Saksham Luthra <sakshamluthra2016@gmail.com>
2026-02-07 23:20:54 +0530

    Hero Section Updated

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -4,6 +4,13 @@ import { FaAngleDown } from "react-icons/fa6";
 import { FaDownload } from "react-icons/fa6";
 
 const Hero = () => {
+  const handleViewWork = () => {
+    const element = document.getElementById("projects");
+    if (element) {
+      element.scrollIntoView();
+    }
+  };
+
   return (
     <>
       <div id="hero" className="h-150 w-full bg-[#020617] relative">
@@ -16,7 +23,7 @@ const Hero = () => {
           <div className="w-full flex flex-col items-center justify-center gap-5 mt-15 sm:mt-20 md:mt-20 lg:mt-30 xl:mt-30 px-5">
             <h1 className="flex items-center px-5 py-1 rounded-2xl text-xs font-semibold bg-[#0cc65914] text-[#0cc65a] border border-[#0cc65937] w-fit gap-2">
               <FaCircle className="text-[11px] fade-in-alternate" />
-              AVAILABLE FOR WORK
+              WEB DEVELOPER / AVAILABLE FOR WORK
             </h1>
             <h2 className="text-5xl sm:text-[65px] md:text-[70px] text-center font-bold w-[80%] lg:w-[80%] xl:w-[70%] mt-4">
               Building digital experiences that{" "}
@@ -29,7 +36,10 @@ const Hero = () => {
               pixel-perfect web applications with React and Tailwind CSS.
             </h3>
             <div className="flex flex-col sm:flex-row gap-5 items-center justify-center w-[65%] md:w-[50%] lg:w-[40%] xl:w-[30%] mt-10 md:mt-5">
-              <button className="h-11 w-full flex items-center justify-center gap-2 primary_button px-4 ">
+              <button
+                className="h-11 w-full flex items-center justify-center gap-2 primary_button px-4 "
+                onClick={handleViewWork}
+              >
                 View Work <FaAngleDown />
               </button>
               <button className="h-11 w-full font-semibold flex border-2 px-4 gap-2 items-center justify-center rounded-lg cursor-pointer leading-normal tracking-[0.030em] border-card-border">
